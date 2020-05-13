954c70d6a8d4cf6c61d2f5b988a366e5a402c976
Titusspear <titus-@hotmail.co.uk>
2020-05-13 18:30:37 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -11,4 +11,4 @@ myOtherBox,object3D.rotation.y += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.y); 
 }
 
-setInterval(spin, 16); //equivalent to 60 fps
+setInterval(spin, 100); //equivalent to 60 fps
