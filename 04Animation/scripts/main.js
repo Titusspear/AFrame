59ba30946d5db6fe9603b34a69e410c44615fb9f
Titusspear <titus-@hotmail.co.uk>
2020-05-13 18:38:05 +0200

    updating index and main

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,14 +1,20 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.09;
+var rotationSpeed = 0.15;
 var myOtherBox = document.getElementById('myOtherBox');
+var myBox = document.getElementById('myBox');
 
 function spin(){
 myOtherBox.object3D.rotation.x += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
 myOtherBox,object3D.rotation.y += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.y); 
+}
+
+function spin(){
+	myBox.object3D.rotation.y += rotationSpeed * 3;
+		console.log(myBox.object3D.rotation.y);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
